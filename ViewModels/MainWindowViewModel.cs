14770ab2c455fa309e48f1775d6fb08b56b6eb99
kritzerenkrieg <kentaro.keren@gmail.com>
2025-10-21 01:20:57 +0700

    initiation of base .NET 8 + Avalonia

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace FastDLX.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "Welcome to Avalonia!";
+}
---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace FastDLX.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
